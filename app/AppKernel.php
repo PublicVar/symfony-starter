ec2da7b067e4ee582d0fde6f46f3ad176042162a
PublicVar <dev.publicvar@gmail.com>
2017-09-29 08:48:51 +0400

    Installation Sonata User

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,10 +18,14 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             //Sonata Admin
-          new Sonata\CoreBundle\SonataCoreBundle(),
-          new Sonata\BlockBundle\SonataBlockBundle(),
-          new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-          new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
